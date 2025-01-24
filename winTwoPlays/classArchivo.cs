7ea18e096ec9f6e1e6850ce29bcc687a99300c07
Jose Luis <joseluisjlgd123@gmail.com>
2025-01-24 12:09:43 -0500

    Acabe la interface

---
+++ winTwoPlays/classArchivo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winTwoPlays
+{
+    internal class classArchivo
+    {
+        public int Num { get; set; }
+        public string Nombre { get; set; }
+        public string Ruta { get; set; }
+        public long Tamaño { get; set; }
+        public long Avance { get; set; }
+        public Boolean Activo { get; set; }
+
+    }
+}
